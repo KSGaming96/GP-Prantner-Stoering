bc04bc3b7dbf87f44ce5d9d0aa49822d6d4c36ff
“NamasteGeek ”git config --global user.email “techchic90@gmail.com ”git config --global user.name “NamasteGeek <40542070+namastegeek@users.noreply.github.com>
2021-04-05 17:02:08 -0500

    Create wendy_Prantner.js
    yummy pizza branch

---
+++ wendy_Prantner.js
@@ -0,0 +1,10 @@
+//best code snippet EVER!
+
+function gitHub () {
+    if (teamRepo !== null) {
+        console.log("Complete Assignment.");
+    } else {
+        console.log("Create Private Repo.");
+    }
+    gitHub(pizzaYum);
+}
